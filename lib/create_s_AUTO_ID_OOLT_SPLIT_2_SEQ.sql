04af482fcd15ef3629bd755fdcdcca934e39730f
Johan Louwers <louwersj@gmail.com>
2014-11-01 16:30:35 +0100

    Create create_s_AUTO_ID_OOLT_SPLIT_2_SEQ.sql
    initial upload

---
+++ lib/create_s_AUTO_ID_OOLT_SPLIT_2_SEQ.sql
@@ -0,0 +1,33 @@
+/* NAME:
+ * create_s_AUTO_ID_OOLT_SPLIT_2_SEQ.sql
+ *
+ * DESC:
+ * create sequence AUTO_ID_OOLT_SPLIT_2_SEQ
+ * 
+ * LOG:
+ * VERSION---DATE--------NAME-------------COMMENT
+ * 0.1       01NOV2014   Johan Louwers    Initial upload to github.com
+ *
+ * LICENSE:
+ * Copyright (C) 2014  Johan Louwers
+ * 
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+ 
+CREATE SEQUENCE "AUTO_ID_OOLT_SPLIT_2_SEQ" 
+MINVALUE 1 MAXVALUE 9999999999999999999999999999 
+INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER NOCYCLE;
+/
